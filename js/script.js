0162b9665a14a5877a27103e9d3f262d7abcf454
ZuhalDesign01 <zainabzohalbalkhi@gmail.com>
2025-07-07 17:49:39 +0430

    update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const hamburger = document.getElementById("hamburger");
+//   const nav = document.getElementById("main-nav");
+
+//   hamburger.addEventListener("click", () => {
+//     nav.classList.toggle("active");
+//   });
+
+//   lucide.createIcons();
+// });
